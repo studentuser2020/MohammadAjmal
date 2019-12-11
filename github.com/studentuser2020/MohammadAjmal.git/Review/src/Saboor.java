fd751e48f2e6a058d65adda2d19ab2ed8a774d2d
Ajmal Ahmadzai <Ajmal Ahmadzai@Ahmadzai>
2019-12-11 03:21:36 -0500

    Added one more sop

--- github.com/studentuser2020/MohammadAjmal.git/Review/src/Saboor.java
+++ github.com/studentuser2020/MohammadAjmal.git/Review/src/Saboor.java
@@ -4,6 +4,8 @@ public class Saboor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("From Saboor Class");
+		
+		System.out.println("Added one more sop ");
 	}
 
 }
